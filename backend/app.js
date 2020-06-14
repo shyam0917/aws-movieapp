740037d437ffb63a26690d3a31172289254fcab6
Shyam <shyam1995saini@gmail.com>
2020-06-14 12:58:03 +0530

    build fixed

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'client/dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
